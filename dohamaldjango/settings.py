80a5046bad9acdff69cb8a1cb46ef69c6fa82d4a
Newval <s8704173@army.idf.il>
2023-05-23 13:12:20 +0300

    new4

--- dohamaldjango/settings.py
+++ dohamaldjango/settings.py
@@ -163,7 +163,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ORIGIN_WHITELIST = ['dohamal-django-dohamal.apps.marganit.openshift.idf']
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
